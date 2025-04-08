1fc7091623a90364ae1aedadec357e5a135b6a7c
sreedhar <jaisriandugala207@gmail.com>
2025-04-08 20:56:03 +0530

    my first commit

---
+++ src/main/java/com/sri/controller/Mycontroller.java
@@ -0,0 +1,16 @@
+package com.sri.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class Mycontroller {
+
+	@GetMapping("/welcome")
+	public String welcomemethod() {
+		return new String("welcome Mr.Rishikumar");
+	}
+	
+	
+}
