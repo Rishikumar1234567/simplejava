5f03f743b9c379afb7d576bb185eed2804f712fc
sreedhar <jaisriandugala207@gmail.com>
2025-04-18 22:22:13 +0530

    my s comit

--- src/main/java/com/sri/controller/Mycontroller.java
+++ src/main/java/com/sri/controller/Mycontroller.java
@@ -9,7 +9,7 @@ public class Mycontroller {
 
 	@GetMapping("/welcome")
 	public String welcomemethod() {
-		return new String("welcome Mr.Bala");
+		return new String("welcome Mr.Seetha");
 	}
 	
 	
